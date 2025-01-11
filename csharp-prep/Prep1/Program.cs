dd0c6c8a1b4bfd5aa3a19fe16bfaeae40bb5a143
TheAJ88 <akshatjohanan@gmail.com>
2025-01-11 14:17:57 -0700

    Finished prep01 assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string _firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string _lastName = Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {_lastName}, {_firstName} {_lastName}.");
     }
 }
